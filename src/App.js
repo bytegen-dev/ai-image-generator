32412d3e888fad6918e43747017ad7e0eb8dd2a9
tertiux44 <secondsonofadebayo@gmail.com>
2023-10-16 00:52:12 +0100

    Updated some .env stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
 import ImageGenerator from './components/imageGenerator/ImageGenerator';
 
 function App() {
+  useEffect(()=>{
+    window.addEventListener("contextmenu", (e)=>{
+      e.preventDefault()
+    })
+  },[])
+
   return (
     <div className='app'>
       <ImageGenerator />
