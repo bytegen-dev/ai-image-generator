1ffa909a1e5aca66186180d4ad2fc28714f5cce2
tertiux44 <secondsonofadebayo@gmail.com>
2023-10-16 00:17:33 +0100

    Updated some .env stuff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register("/service-worker.js")
+  .then((registration)=>{
+    console.log("Service worker registered with scope:", registration.scope)
+  })
+  .catch((error)=>{
+    console.error("Service Worker registration failed:", error);
+  })
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
